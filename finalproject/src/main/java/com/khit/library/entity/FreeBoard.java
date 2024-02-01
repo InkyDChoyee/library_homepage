738e1e6cad282a3e420d812ced18bf5a402251cb
amuselsj <tndtlflee@naver.com>
2024-02-01 11:04:44 +0900

    자유게시판

--- finalproject/src/main/java/com/khit/library/entity/FreeBoard.java
+++ finalproject/src/main/java/com/khit/library/entity/FreeBoard.java
@@ -4,17 +4,25 @@ import java.sql.Timestamp;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.khit.library.dto.FreeBoardDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "free_board")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FreeBoard extends BaseEntity{
 	
 	
@@ -30,6 +38,23 @@ public class FreeBoard extends BaseEntity{
 	
 	@Column(nullable = true)
 	private Integer hit;  //조회수
-
+	
+	public static FreeBoard toSaveEntity(FreeBoardDTO fboardDTO) {
+		FreeBoard fboard = FreeBoard.builder()
+				.title(fboardDTO.getTitle())
+				.content(fboardDTO.getContent())
+				.hit(fboardDTO.getHit())
+				.build();
+		return fboard;
+	}
+	public static FreeBoard toUpdateEntity(FreeBoardDTO fboardDTO) {
+		FreeBoard fboard = FreeBoard.builder()
+				.id(fboardDTO.getId())
+				.title(fboardDTO.getTitle())
+				.content(fboardDTO.getContent())
+				.hit(fboardDTO.getHit())
+				.build();
+		return fboard;
+	}
 
 }
