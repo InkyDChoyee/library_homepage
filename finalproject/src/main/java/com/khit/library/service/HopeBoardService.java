efb1c9d665102cccd15108516c9e3f049285e9a4
kimheerin <hheerriinn@gmail.com>
2024-02-07 15:17:16 +0900

    15:17 push

--- finalproject/src/main/java/com/khit/library/service/HopeBoardService.java
+++ finalproject/src/main/java/com/khit/library/service/HopeBoardService.java
@@ -1,21 +1,20 @@
 package com.khit.library.service;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.khit.library.dto.HopeBoardDTO;
+import com.khit.library.dto.NoticeBoardDTO;
 import com.khit.library.entity.HopeBoard;
+import com.khit.library.entity.NoticeBoard;
 import com.khit.library.repository.HopeBoardRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -23,21 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class HopeBoardService {
 	private final HopeBoardRepository hopeBoardRepository;
 
-	public void save(HopeBoard hopeBoard, MultipartFile hopeBoardFile) throws Exception, IOException {
-
-		if(!hopeBoardFile.isEmpty()) { 
-		/*String hopeFilepath = "C:\\Final_project\\final-project\\finalproject\\src\\main\\resources\\static\\upload\\";*/
-		String hopeFilepath = "C:\\final-project\\finalproject\\src\\main\\resources\\static\\upload";
-    UUID uuid = UUID.randomUUID();
-		String hopeFilename = uuid + "_" + hopeBoardFile.getOriginalFilename();
-		
-		File savedHopeFile = new File(hopeFilepath, hopeFilename);
-		hopeBoardFile.transferTo(savedHopeFile);
-		
-		hopeBoard.setHopeFilename(hopeFilename);
-		hopeBoard.setHopeFilepath("/upload/" + hopeFilename);
-		}
-		
+	public void save(HopeBoard hopeBoard) {
 		hopeBoardRepository.save(hopeBoard);
 	}
 
@@ -63,28 +48,14 @@ public class HopeBoardService {
 		hopeBoardRepository.deleteById(hbid);
 	}
 
-	public HopeBoardDTO update(HopeBoardDTO hopeBoardDTO, MultipartFile hopeBoardFile) throws Exception, IOException {
-		if(!hopeBoardFile.isEmpty()) {
-			String hopeFilepath = "C:\\final-project\\finalproject\\src\\main\\resources\\static\\upload";
-			UUID uuid = UUID.randomUUID();
-			String hopeFilename = uuid + "_" + hopeBoardFile.getOriginalFilename();
-			
-			File savedHopeFile = new File(hopeFilepath, hopeFilename);
-			hopeBoardFile.transferTo(savedHopeFile);
-			
-			hopeBoardDTO.setHopeFilename(hopeFilename);
-			hopeBoardDTO.setHopeFilepath("/upload/" + hopeFilename);
-		}else {
-			hopeBoardDTO.setHopeFilename(findById(hopeBoardDTO.getHbid()).getHopeFilename());
-			hopeBoardDTO.setHopeFilepath(findById(hopeBoardDTO.getHbid()).getHopeFilepath());
-		}
+	public void update(HopeBoardDTO hopeBoardDTO) {
 		HopeBoard hopeBoard = HopeBoard.toUpdateEntity(hopeBoardDTO);
 		hopeBoardRepository.save(hopeBoard);
-		return findById(hopeBoardDTO.getHbid());
 	}
-	
-	@Transactional
-	public void updateHits(Long hbid) {
-		hopeBoardRepository.updateHits(hbid);
-	}
+
+	//페이징
+	public Page<HopeBoardDTO> paging(Pageable pageable) {
+		Page<HopeBoard> hopeBoardPage = hopeBoardRepository.findAll(pageable);
+        return hopeBoardPage.map(hopeBoard -> HopeBoardDTO.toSaveDTO(hopeBoard));
+    }
 }
