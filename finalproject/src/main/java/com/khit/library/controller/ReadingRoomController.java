5d8a82618b0c33adc72d4ee5c910cdf5a9028c11
jsk172 <jstar17@naver.com>
2024-02-14 18:03:11 +0900

    readingroom

--- finalproject/src/main/java/com/khit/library/controller/ReadingRoomController.java
+++ finalproject/src/main/java/com/khit/library/controller/ReadingRoomController.java
@@ -1,9 +1,19 @@
 package com.khit.library.controller;
 
+import com.khit.library.config.SecurityUser;
+import com.khit.library.dto.MemberDTO;
+import com.khit.library.dto.ReadingRoomDTO;
+import com.khit.library.entity.Member;
+import com.khit.library.service.MemberService;
 import com.khit.library.service.ReadingRoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,4 +21,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReadingRoomController {
 
     private final ReadingRoomService readingRoomService;
+    private final MemberService memberService;
+
+    //좌석
+    @GetMapping("/room")
+    public String getList(Model model, @AuthenticationPrincipal SecurityUser principal){
+        List<ReadingRoomDTO> readingRoomDTOList = readingRoomService.findAll();
+        model.addAttribute("seatList", readingRoomDTOList);
+        if(principal == null){
+            return "readingroom/room";
+        }else{
+            MemberDTO memberDTO = memberService.findByMid(principal);
+            model.addAttribute("member", memberDTO);
+            return "readingroom/room";
+        }
+    }
+
+    //좌석 선택
+    @PostMapping("/select")
+    public ReadingRoomDTO select(@RequestParam("readingId") Long readingId, @AuthenticationPrincipal SecurityUser principal){
+        ReadingRoomDTO readingRoomDTO = readingRoomService.findById(readingId);
+
+        MemberDTO memberDTO = memberService.findByMid(principal);
+        readingRoomDTO.setMember(Member.builder().memberId(memberDTO.getMemberId()).build());
+
+        if(readingRoomDTO.getEnter() == null){
+            readingRoomDTO.setEnter(new Timestamp(System.currentTimeMillis()));
+        }
+        readingRoomDTO.setSeatAvailable(false);
+        readingRoomService.save(readingRoomDTO);
+
+        /*readingRoomDTO.setEnter(new Timestamp(System.currentTimeMillis()));*/
+        return readingRoomDTO;
+    }
 }
--- finalproject/src/main/java/com/khit/library/service/ReadingRoomService.java
+++ finalproject/src/main/java/com/khit/library/service/ReadingRoomService.java
@@ -1,11 +1,36 @@
 package com.khit.library.service;
 
+import com.khit.library.dto.ReadingRoomDTO;
+import com.khit.library.entity.ReadingRoom;
 import com.khit.library.repository.ReadingRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ReadingRoomService {
     private final ReadingRoomRepository readingRoomRepository;
+
+    //좌석
+    public List<ReadingRoomDTO> findAll() {
+        List<ReadingRoom> readingRoomList = readingRoomRepository.findAll();
+        List<ReadingRoomDTO> readingRoomDTOList = new ArrayList<>();
+
+        for(ReadingRoom readingRoom : readingRoomList){
+            ReadingRoomDTO readingRoomDTO = ReadingRoomDTO.toSaveDTO(readingRoom);
+            readingRoomDTOList.add(readingRoomDTO);
+        }
+        return readingRoomDTOList;
+    }
+
+    public void save(ReadingRoomDTO readingRoomDTO) {
+
+    }
+
+    public ReadingRoomDTO findById(Long readingId) {
+        return null;
+    }
 }
