98c9808b43df9145e904addc5b45c0dc56c21434
jsk172 <jstar17@naver.com>
2024-02-18 21:03:54 +0900

    fix

--- finalproject/src/main/java/com/khit/library/controller/LoginController.java
+++ finalproject/src/main/java/com/khit/library/controller/LoginController.java
@@ -4,17 +4,24 @@ import com.khit.library.service.NaverService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RequiredArgsConstructor
 @Controller
 public class LoginController {
+
+    //    private final AppleService appleService;
+//    private final KakaoService kakaoService;
     private final NaverService naverService;
 
-    @GetMapping("/naverLogin")
-    public String socialLogin(Model model){
+    @RequestMapping(value="/naver/login", method= RequestMethod.GET)
+    public String login(Model model) {
+//        model.addAttribute("appleUrl", appleService.getAppleLogin());
+//        model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
         model.addAttribute("naverUrl", naverService.getNaverLogin());
 
-        return "/";
+        return "redirect:/";
     }
+
 }
